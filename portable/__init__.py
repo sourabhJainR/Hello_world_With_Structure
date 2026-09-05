8e3c327bbd48ccab5b451009b4003914bd30b726
Sourabh Jain <sourjain2002@gmail.com>
2026-09-05 19:49:21 +0530

    feat: expose adaptive runtime facade

--- portable/__init__.py
+++ portable/__init__.py
@@ -1,5 +1,6 @@
 """Portable AER runtime package."""
 
+from .adaptive_runtime import AdaptiveRuntime
 from .aer_runtime import main
 from .lifecycle_hooks import HookBus, HookDecision, HookEvent, HookPhase, HookedExecution
 from .provider_fabric import CapabilityRequest, ProviderCapability, ProviderFabric, RoutingDecision
@@ -7,6 +8,7 @@ from .session_state import SessionCheckpoint, SessionStore
 
 __all__ = [
     "main",
+    "AdaptiveRuntime",
     "HookBus",
     "HookDecision",
     "HookEvent",
