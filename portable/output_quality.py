b542e606a7ab1156c16f0d1f50913f143552c20b
Sourabh Jain <sourjain2002@gmail.com>
2026-09-14 03:23:34 +0530

    feat: expose canonical output quality gate

---
+++ portable/output_quality.py
@@ -0,0 +1,4 @@
+"""Stable output-quality import surface backed by AER's unified capability core."""
+from .agent_capabilities import OutputQualityGate, QualityResult
+
+__all__ = ["OutputQualityGate", "QualityResult"]
