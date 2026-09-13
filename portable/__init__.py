d7ba3c1018755e667582a5cf737a6075d5178eeb
Sourabh Jain <sourjain2002@gmail.com>
2026-09-14 03:08:40 +0530

    feat: expose unified capability layer

--- portable/__init__.py
+++ portable/__init__.py
@@ -2,6 +2,12 @@
 
 from .adaptive_runtime import AdaptiveRuntime
 from .aer_runtime import main
+from .hermes_capabilities import (
+    CAPABILITY_NAMES, TOOLSET_PRESETS, CapabilityRegistry, CapabilitySpec, CapabilityState,
+    CronJob, CronStore, DelegationManager, DelegationReceipt, HermesCapabilityRuntime,
+    MemoryEntry, MemoryMutation, MemoryStore, OutputQualityGate, ProcessManager, ProcessReceipt,
+    QualityFinding, QualityReport, SessionMessage, SkillRegistry, TerminalBackends, TerminalCommand,
+)
 from .impact_analysis import ImpactRecord, ImpactReport, analyze
 from .lifecycle_hooks import HookBus, HookDecision, HookEvent, HookPhase, HookedExecution
 from .provider_fabric import CapabilityRequest, ProviderCapability, ProviderFabric, RoutingDecision
@@ -9,24 +15,11 @@ from .session_state import SessionCheckpoint, SessionStore
 from .task_planner import PRIORITIES, STATUSES, Task, TaskPlan
 
 __all__ = [
-    "main",
-    "AdaptiveRuntime",
-    "HookBus",
-    "HookDecision",
-    "HookEvent",
-    "HookPhase",
-    "HookedExecution",
-    "CapabilityRequest",
-    "ProviderCapability",
-    "ProviderFabric",
-    "RoutingDecision",
-    "SessionCheckpoint",
-    "SessionStore",
-    "ImpactRecord",
-    "ImpactReport",
-    "analyze",
-    "Task",
-    "TaskPlan",
-    "STATUSES",
-    "PRIORITIES",
+    "main", "AdaptiveRuntime", "HookBus", "HookDecision", "HookEvent", "HookPhase", "HookedExecution",
+    "CapabilityRequest", "ProviderCapability", "ProviderFabric", "RoutingDecision", "SessionCheckpoint", "SessionStore",
+    "ImpactRecord", "ImpactReport", "analyze", "Task", "TaskPlan", "STATUSES", "PRIORITIES",
+    "CAPABILITY_NAMES", "TOOLSET_PRESETS", "CapabilityRegistry", "CapabilitySpec", "CapabilityState",
+    "MemoryStore", "MemoryEntry", "MemoryMutation", "SessionMessage", "SkillRegistry", "ProcessManager", "ProcessReceipt",
+    "TerminalBackends", "TerminalCommand", "DelegationManager", "DelegationReceipt", "CronStore", "CronJob",
+    "OutputQualityGate", "QualityFinding", "QualityReport", "HermesCapabilityRuntime",
 ]
