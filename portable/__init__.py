063d9c9588051b6f103ded7d3379c4c84bc3ec20
Sourabh Jain <sourjain2002@gmail.com>
2026-09-14 03:24:42 +0530

    feat: expose unified agent capability surfaces

--- portable/__init__.py
+++ portable/__init__.py
@@ -7,26 +7,15 @@ from .lifecycle_hooks import HookBus, HookDecision, HookEvent, HookPhase, Hooked
 from .provider_fabric import CapabilityRequest, ProviderCapability, ProviderFabric, RoutingDecision
 from .session_state import SessionCheckpoint, SessionStore
 from .task_planner import PRIORITIES, STATUSES, Task, TaskPlan
+from .capability_fabric import CAPABILITIES, Capability, CapabilityFabric
+from .persistent_memory import MemoryRecord, PersistentMemory
+from .automation_scheduler import AutomationScheduler, RunClaim, Schedule
+from .output_quality import OutputQualityGate, QualityResult
 
 __all__ = [
-    "main",
-    "AdaptiveRuntime",
-    "HookBus",
-    "HookDecision",
-    "HookEvent",
-    "HookPhase",
-    "HookedExecution",
-    "CapabilityRequest",
-    "ProviderCapability",
-    "ProviderFabric",
-    "RoutingDecision",
-    "SessionCheckpoint",
-    "SessionStore",
-    "ImpactRecord",
-    "ImpactReport",
-    "analyze",
-    "Task",
-    "TaskPlan",
-    "STATUSES",
-    "PRIORITIES",
+    "main", "AdaptiveRuntime", "HookBus", "HookDecision", "HookEvent", "HookPhase", "HookedExecution",
+    "CapabilityRequest", "ProviderCapability", "ProviderFabric", "RoutingDecision", "SessionCheckpoint", "SessionStore",
+    "ImpactRecord", "ImpactReport", "analyze", "Task", "TaskPlan", "STATUSES", "PRIORITIES",
+    "CAPABILITIES", "Capability", "CapabilityFabric", "MemoryRecord", "PersistentMemory",
+    "AutomationScheduler", "RunClaim", "Schedule", "OutputQualityGate", "QualityResult",
 ]
