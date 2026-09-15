ee07fc6b781b9a8b79efcd85127f881c62ccad71
Sourabh Jain <sourjain2002@gmail.com>
2026-09-16 00:41:11 +0530

    feat: expose repository intelligence runtime

--- portable/__init__.py
+++ portable/__init__.py
@@ -18,6 +18,7 @@ from .feedback_loop import BoundedLoop, LoopAction, LoopDefinition, LoopPass, Lo
 from .agency_provenance import ProvenanceLedger, ProvenanceRecord
 from .engineering_design_guard import DesignDimension, DesignFinding, DesignReviewReceipt, EngineeringDesignGuard, FindingSeverity
 from .agency_state_graph import Checkpoint, CompiledStateGraph, GraphEvent, GraphInterrupt, GraphRun, InMemoryCheckpointStore, RetryPolicy, StateGraph
+from .repo_intelligence import Edge, Evidence, FileRecord, RepoAnswer, RepositoryMap, Symbol, render_compact
 from .agent_patterns import (
     ConsensusResult,
     EvidenceItem,
@@ -48,6 +49,7 @@ __all__ = [
     "ProvenanceLedger", "ProvenanceRecord", "DesignDimension", "DesignFinding", "DesignReviewReceipt",
     "EngineeringDesignGuard", "FindingSeverity", "Checkpoint", "CompiledStateGraph", "GraphEvent",
     "GraphInterrupt", "GraphRun", "InMemoryCheckpointStore", "RetryPolicy", "StateGraph",
+    "Symbol", "FileRecord", "Edge", "Evidence", "RepoAnswer", "RepositoryMap", "render_compact",
     "Specialist", "SpecialistRouter", "RouteRequest", "RouteDecision",
     "ResearchTask", "ResearchPlan", "ResearchPlanner", "EvidenceItem", "ConsensusResult", "MixtureOfAgents",
     "OptimizationRound", "OptimizationResult", "OneChangeOptimizer", "ScopeFinding", "ScopeReport", "ScopeChecker",
